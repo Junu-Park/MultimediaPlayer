9def61e73fc79a641c5a31584697a39ab510bf29
arthur <arthur@tourlive.co.kr>
2026-04-17 11:48:26 +0900

    멀티미디어 플레이어 Xcode 프로젝트 초기 구성

---
+++ MultimediaPlayer/MultimediaPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultimediaPlayerApp.swift
+//  MultimediaPlayer
+//
+//  Created by tourlive_arthur on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MultimediaPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
